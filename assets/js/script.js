4df9e6dff316a65cb0ca04f114d1b023a6bc0110
AngerOverApathy <angersavestheworld@gmail.com>
2022-01-27 13:23:53 -0800

    added colored time blocks and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,49 @@ var fourPm = $("#16pm");
 var fivePm = $("#17pm");
 
 //moment current hour/day/month/year/day of week/time
-var currentDate = moment().format("Do MMM YYYY") + " " + moment().format('dddd');
+var currentDate = moment().format("dddd") + " " + moment().format('MMM Do YYYY');
 var currentTime = moment().format('h:mm:ss a');
+
+//color blocks
+$(document).ready(function() {
+  $('.saveBtn').on('click', function() {
+    var time =$(this).siblings('.form-control').attr('id');
+    var task = $(this).siblings('.form-control').val();
+
+    window.localStorage.setItem(time, task)
+  })
+
+//check time for color blocking
+function checkHour() {
+  $('.form-control').each(function() {
+    var currentHour = moment().hours();
+    var blockHour = $(this).attr('id').split('');
+    var newHour = parseInt(blockHour[0]+blockHour[1])
+  
+    if(newHour < currentHour) {
+      $(this).addClass('past')
+    } else if(newHour === currentHour) {
+      $(this).addClass('present')
+      $(this).removeClass('past')
+    } else {
+      $(this).addClass('future')
+      $(this).removeClass('past')
+      $(this).removeClass('present')
+    }
+  })
+}
+
+
+  //clear calendar
+  $("#clearCal").on("click", function(){
+    window.localStorage.clear();
+    window.location.reload();
+  });
+
+  checkHour();
+  var checkInterval = setInterval(checkHour, 15000)
+})
+
 
 //time+day function
 var calendar = setInterval(function() {
@@ -24,18 +65,11 @@ var calendar = setInterval(function() {
   }, 100);
 
 
-//clear calendar
-$("#clearCal").on("click", function(){
-    localStorage.clear();
-    initPage()
-  });
-
-//time blocks
-
-//color coded time blocks (past/present/future)
-
-//click block to add event
-
-//save button
-
-//save in local storage
+//saving to local storage
+for(let i =8 ; i<= 17; i++) {
+  if(i<12) {
+    $('#' + i + 'am').val(localStorage.getItem(i + 'am'))
+  } else {
+    $('#' + i + 'pm').val(localStorage.getItem(i + 'pm'))
+  }
+}
